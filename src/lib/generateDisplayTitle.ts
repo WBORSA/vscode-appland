c78d6e07fb4244a35f4649bd4a63a4b4e5429134
MaxDillon <maxdillon13124@gmail.com>
2022-06-23 14:28:37 -0700

    changed formatting when undefined path or lineno

--- src/lib/generateDisplayTitle.ts
+++ src/lib/generateDisplayTitle.ts
@@ -4,12 +4,14 @@ import * as vscode from 'vscode';
 // Gets's name displayed in Findings bar
 export default (finding: ResolvedFinding): string => {
   const absPath = finding.problemLocation?.uri.path;
-  const relPath = absPath ? vscode.workspace.asRelativePath(absPath) : '';
+  const relPath = absPath ? vscode.workspace.asRelativePath(absPath) : undefined;
 
   const rule = finding.finding.ruleTitle;
   const context = finding.finding.groupMessage || finding.finding.message;
   const path = finding.finding.scope.path || relPath;
-  const lineno = finding.finding.scope.lineno || finding.problemLocation?.range.start.line || '';
+  const lineno = finding.finding.scope.lineno || finding.problemLocation?.range.start.line;
 
-  return `${rule}: ${context}, ${path}:${lineno}`;
+  const fullPathString = path && lineno ? `, ${path}:${lineno}` : '';
+
+  return `${rule}: ${context}${fullPathString}`;
 };
